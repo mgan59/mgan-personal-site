ac9e24d3b7ba72e2b4e6a345d27b4e5c1caf4cae
Morgan Craft <mgan59@gmail.com>
2020-11-21 07:07:30 -0500

    wired google-font into head

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,31 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 import styled from 'styled-components'
 
 const TestHeader = styled.h1`
-  color: #ff0000;
+  color: #333;
+  font-family: 'Lora', serif;
+`
+
+const OtherHeader = styled.h2`
+font-family: 'Roboto Slab', serif;
 `
 export default function Home () {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Morgan Craft</title>
         <link rel='icon' href='/favicon.ico' />
+        {/* Provided by g-font in recent embed tool, think this helps cdn cache the fonts */}
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        {/* Pair down the font-glyphs post design, don't need all weights */}
+        <link href='https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Roboto+Slab:wght@100;300;700display=swap' rel='stylesheet' />
       </Head>
 
-      <main className={styles.main}>
-        <TestHeader>TESTERR!!!</TestHeader>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/master/examples'
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <TestHeader>Hello!!</TestHeader>
+        <OtherHeader>Kaboom!</OtherHeader>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }

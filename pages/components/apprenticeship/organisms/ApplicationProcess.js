4cbe39a7fccc1bd2d1a2eb03b23b607c591415f0
Morgan Craft <mgan59@gmail.com>
2021-07-13 08:00:07 -0400

    content(apprenticeship): adjusting start date

--- pages/components/apprenticeship/organisms/ApplicationProcess.js
+++ pages/components/apprenticeship/organisms/ApplicationProcess.js
@@ -58,7 +58,7 @@ const ApplicationProcess = (props) => {
     <Frame>
       <SectionHeader textAlign='center'>SOUNDS GREAT! How Can I Apply?</SectionHeader>
       <TightTextParagraph fontWeight='400'><strong>ENROLLMENT IS CURRENTLY OPEN</strong> -- <em>Limited Spots Available</em></TightTextParagraph>
-      <TightTextParagraph fontWeight='400'>Summer Cohort Begins (July 5th)</TightTextParagraph>
+      <TightTextParagraph fontWeight='400'>Summer Cohort Begins (August 16th)</TightTextParagraph>
       <TextParagraph>With only 2-3 spots left, you will want to complete an application to get started today! <br /><br />
       All applications are reviewed within 24-48 hours. Once reviewed there will be a follow-up to schedule a 30 minute interview to make sure you are a good fit.
       </TextParagraph>
